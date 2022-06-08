1dae74f2f325ddafb8bca1db76e2c7e2b207eb46
Taylor <taylor.oliveira@tcm.ba.gov.br>
2022-06-08 15:33:38 -0300

    editing for ejs

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 9500
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (request, response) => {
+    console.log("Requisição a página principal recebida!")
+    response.render('index')
+})
+
+app.get('/login', (request, response) => {
+    console.log("Requisição a página de login recebida!")
+    response.render('pages/entrar')
+})
+
+app.get('/adicionar', (request, response) => {
+    console.log("Requisição da página adicionar recebida!")
+    response.render('pages/adicionar')
+})
+
+app.get('/sobre', (request, response) => {
+    console.log("Requisição da página sobre recebida!")
+    response.render('pages/sobre')
+})
+
+app.listen(port, () => {
+    console.log(`Serviço rodando na porta: ${port}`)
+})
